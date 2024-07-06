0afd51306196fc34a58ca693c611679f6d0af67d
tiagoluissilvati <tiagoluissilvati@gmail.com>
2024-07-05 23:06:41 -0300

    remover o loading splash

--- weasis-launcher/src/main/java/org/weasis/launcher/WeasisLauncher.java
+++ weasis-launcher/src/main/java/org/weasis/launcher/WeasisLauncher.java
@@ -238,7 +238,7 @@ public class WeasisLauncher {
       mFelix.start();
 
       // End of splash screen
-      loader.close();
+     // loader.close();
       loader = null;
 
       String logActivation = serverProp.get("org.apache.sling.commons.log.file");
@@ -968,7 +968,7 @@ public class WeasisLauncher {
     // Splash screen that shows bundles loading
     final WeasisLoader loader = new WeasisLoader(cacheDir.toPath(), mainFrame);
     // Display splash screen
-    loader.open();
+    //loader.open();
 
     if (versionNew != null) {
       localSourceProp.put(P_WEASIS_VERSION, versionNew);
